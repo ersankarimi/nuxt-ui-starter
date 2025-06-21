56df3f43b15bc815a6b467a4353e131ed5cb7ff4
Ersan Karimi <ersankarimii311@gmail.com>
2025-06-21 15:06:55 +0800

    refactor: restructure index.vue for improved layout and fix Nuxt UI Docs link

--- pages/index.vue
+++ pages/index.vue
@@ -6,11 +6,20 @@ const site = useSiteConfig();
   <div
     class="bg-gray-900 h-screen w-screen flex flex-col items-center justify-center gap-5"
   >
-    <h1
-      class="font-bold text-2xl text-primary md:text-3xl"
-    >
-      Welcome to {{ site.name }}
-    </h1>
+    <div class="flex flex-col gap-4 items-center justify-center">
+      <UBadge
+        size="sm"
+        class="w-fit"
+        variant="subtle"
+      >
+        Nuxt 3 + Nuxt UI 3
+      </UBadge>
+      <h1
+        class="font-bold text-2xl text-primary md:text-3xl"
+      >
+        Welcome to {{ site.name }}
+      </h1>
+    </div>
     <div class="flex items-center gap-4">
       <UButton
         label="Nuxt Docs"
@@ -23,7 +32,7 @@ const site = useSiteConfig();
         label="Nuxt UI Docs"
         variant="outline"
         icon="i-heroicons-book-open"
-        to="https://ui2.nuxt.com"
+        to="https://ui.nuxt.com"
         target="_blank"
         external
       />
